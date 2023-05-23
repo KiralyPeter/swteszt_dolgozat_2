43ef7bd90b590fc022952fd86e3f05a5f15232c1
Peter Kiraly <peter.kiral@gmail.com>
2023-05-23 18:20:29 +0200

    tests are working

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,6 +1,5 @@
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +17,21 @@ public class CalcTest {
         double actual = this.mainFrame.calcTime(20, 2, 20);
         double expected = 0.5;
         assertEquals(expected, actual, 0.1);
-
     }
+
+    @Test
+    public void testCalcTime2(){
+        double actual = this.mainFrame.calcTime(1, 1, 1);
+        double expected = 1;
+        assertEquals(expected, actual, 0.1);
+    }
+
+    @Test
+    public void testCalcTime3(){
+        double actual = this.mainFrame.calcTime(100, 100, 100);
+        double expected = 0.01;
+        assertEquals(expected, actual, 0.1);
+    }
+
     
 }
